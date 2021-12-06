978e39b49782243b06642ec4c3494cd4d32af594
KieranWalsh <walshk@pm.me>
2021-12-06 23:37:35 +0000

    Added comment-based help.

--- Update-AllPSModules/Update-AllPSModules.ps1
+++ Update-AllPSModules/Update-AllPSModules.ps1
@@ -1,9 +1,32 @@
 ﻿<#
+    .SYNOPSIS
+    This script will update all locally installed PowerShell modules to the newest ones if can find online.
+    .DESCRIPTION
+    The script will search the usual PowerShell module profile paths for all modules and update them to the newest versions available online.
+
+    Updating modules depends on ‘PackageManagement’ and ‘PowerShellGet’, which are updated to the newest versions before upgrading any modules.
+    By default, it searches for beta, nightly, preview versions, etc., but you can exclude those with the “-NoPreviews” switch.
+
+    The script presents you with a list of all modules it finds and shows you if a newer version is detected and when that new version was published.
+
+    PowerShell comes with a similar “Update-Module” command, but that does not try to update ‘PackageManagement’ and ‘PowerShellGet’.
+    It shows no data while operating, so you are left with an empty screen unless you use the “-verbose” switch, which displays too much information.
+    You can use the “-AllowPrerelease”, but only with a named module. This script will install Prerelease versions of all modules if they exist.
+
+    .PARAMETER NoPreviews
+    If you want to avoid versions that include 'beta', 'preview', 'nightly', etc.,  and only upgrade to fully released versions of the modules, use this switch.
+    .EXAMPLE
+    Update-AllPSModules
+    This will update all locally installed modules .
+    .EXAMPLE
+    Update-AllPSModules -NoPreviews
+    This will update all locally installed modules but not to versions that include 'beta', 'preview', 'nightly', etc.
+    .NOTES
     Filename:       Update-AllPSModules.ps1
     Contributors:   Kieran Walsh
     Created:        2021-01-09
     Last Updated:   2021-12-06
-    Version:        1.43.01
+    Version:        1.43.02
 #>
 [CmdletBinding()]
 Param(
